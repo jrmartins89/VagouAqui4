06300a3c03ebb59577f330b6fd41bd271cd8a07c
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-08 13:50:47 -0300

    update log messages.

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose
         db,
         { useNewUrlParser: true }
     )
-    .then(() => console.log("MongoDB successfully connected"))
+    .then(() => console.log("Conectado com sucesso ao MongoDB"))
     .catch(err => console.log(err));
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
-app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+app.listen(port, () => console.log(`O Servidor está rodando na porta ${port} !`));
