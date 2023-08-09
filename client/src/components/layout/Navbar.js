affde8e05e41d5859e3f04d0c1b99d31f6fe9df1
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-08 23:43:30 -0300

    Updated information in NavBar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -13,8 +13,8 @@ class Navbar extends Component {
                             }}
                             className="col s5 brand-logo center black-text"
                         >
-                            <i className="material-icons">code</i>
-                            MERN
+                            <i className="material-icons">code</i> //symbol next to the word
+                            Página Inicial
                         </Link>
                     </div>
                 </nav>
