21cf520322701555cce6b65a91f7222ace26d28d
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-14 20:34:15 -0300

    included react hooks and other elements to ProductList.js

--- client/src/components/product-list/ProductList.js
+++ client/src/components/product-list/ProductList.js
@@ -1,7 +1,71 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 
-const ProductList = () =>  {
-    const [filteredProductList] = useState([]);
+const ProductList = ({filteredProductList}) =>  {
+    const [loading, setLoading] = useState(false);
+    const [productList, setProductList] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [selectedCategories, setSelectedCategories] = useState([]);
+    const [fileredProductList, setFilteredProductList] = useState([]);
+    const addCategory = (category) => {
+        if(!selectedCategories.includes(category)){
+            setSelectedCategories(prev => ([...prev, category]))
+        }
+    }
+
+    const removeCategory = (category) => {
+        if(selectedCategories.includes(category)){
+            console.log(selectedCategories)
+            const removedList = selectedCategories.filter((item) => (item !== category));
+            setSelectedCategories(removedList);
+        }
+    }
+
+    const resetCategory = () => {
+        setSelectedCategories([]);
+    }
+
+    useEffect(() => {
+        if(selectedCategories.length === 0){
+            setFilteredProductList(productList);
+        } else{
+            setFilteredProductList(productList.filter((item)=>(selectedCategories.includes(item.category))));
+        }
+    }, [selectedCategories, productList])
+    const getCategories = async () => {
+        setLoading(true);
+
+        await fetch('https://dummyjson.com/products/categories')
+            .then(res => res.json())
+            .then(data => {
+                const arr = data.slice(0, 6);
+                setCategories(arr);
+            })
+            .catch(err => alert(err))
+            .finally(()=>{
+                setLoading(false);
+            })
+    }
+
+    const getProducts = async () => {
+        setLoading(true);
+
+        await fetch('https://dummyjson.com/products')
+            .then(res => res.json())
+            .then(data => {
+                setProductList(data.products);
+                setFilteredProductList(data.products);
+                getCategories(); // get the categories list
+            })
+            .catch(err => alert(err))
+            .finally(()=>{
+                setLoading(false);
+            })
+    }
+
+    useEffect(() => {
+        getProducts();
+        // eslint-disable-next-line
+    }, [])
         return (
             <div className="w-full h-[85%] px-5">
                 <h4>
