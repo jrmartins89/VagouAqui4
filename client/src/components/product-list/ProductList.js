f4f249ec1592437342c5ddf81edc439e57b4a33c
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-14 15:18:15 -0300

    refactor page to change implementation of ProductList

--- client/src/components/product-list/ProductList.js
+++ client/src/components/product-list/ProductList.js
@@ -1,39 +1,50 @@
-import React, { Component } from "react";
+import React, {useState} from "react";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {logoutUser} from "../../actions/authActions";
 
-class ProductList extends Component {
-    onLogoutClick = e => {
-        e.preventDefault();
-        this.props.logoutUser();
-    };
-    render() {
-        const { user } = this.props.auth;
+const ProductList = () =>  {
+    const [filteredProductList, setFilteredProductList] = useState([]);
         return (
-            <div style={{ height: "75vh" }} className="container valign-wrapper">
-                <div className="row">
-                    <div className="col s12 center-align">
-                        <h4>
-                            <b>Hey there,</b> {user.name.split(" ")[0]}
-                        </h4>
-                        <button
-                            style={{
-                                width: "150px",
-                                borderRadius: "3px",
-                                letterSpacing: "1.5px",
-                                marginTop: "1rem"
-                            }}
-                            onClick={this.onLogoutClick}
-                            className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                        >
-                            Logout
-                        </button>
-                    </div>
+            <div className="w-full h-[85%] px-5">
+                <h4>
+                    <b>Hey there,</b>
+                </h4>
+                <button
+                    style={{
+                        width: "150px",
+                        borderRadius: "3px",
+                        letterSpacing: "1.5px",
+                        marginTop: "1rem"
+                    }}
+                    className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                >
+                    Logout
+                </button>
+                <div className="w-full">Anúncios: </div>
+                <div className="w-full h-full flex flex-wrap gap-1 justify-between items-start overflow-y-auto">
+                    {
+                        filteredProductList.map((product) => (
+                            <div key={product.id} className='w-[19%] h-fit my-3 rounded-xl overflow-hidden border border-gray-200'>
+                                <img
+                                    src={product.thumbnail}
+                                    alt='product'
+                                    className='w-full h-28 object-cover'
+                                />
+                                <div className="mt-2 mb-2 px-3">
+                                    <div className="font-semibold">
+                                        {(product.title.length > 25)? product.title.substring(0,22) + '...': product.title}
+                                    </div>
+                                    <div className="text-sm text-gray-600">
+                                        {product.category}
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
-        );
-    }
+        )
 }
 ProductList.propTypes = {
     logoutUser: PropTypes.func.isRequired,
