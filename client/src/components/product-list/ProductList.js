ee95eb0e0444f0b2b52d8dd9914e1824474dd360
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-14 20:25:00 -0300

    refactor code to remove auth elements

--- client/src/components/product-list/ProductList.js
+++ client/src/components/product-list/ProductList.js
@@ -1,7 +1,4 @@
 import React, {useState} from "react";
-import PropTypes from "prop-types";
-import {connect} from "react-redux";
-import {logoutUser} from "../../actions/authActions";
 
 const ProductList = () =>  {
     const [filteredProductList] = useState([]);
@@ -35,14 +32,4 @@ const ProductList = () =>  {
             </div>
         )
 }
-ProductList.propTypes = {
-    logoutUser: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired
-};
-const mapStateToProps = state => ({
-    auth: state.auth
-});
-export default connect(
-    mapStateToProps,
-    { logoutUser }
-)(ProductList);
+export default ProductList;
