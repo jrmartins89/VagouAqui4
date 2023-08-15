d8a2eb9d22be137211b425d99f444359bdd61050
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-15 13:57:50 -0300

    updated folder path name

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import PrivateRoute from "./components/private-route/PrivateRoute";
 import Dashboard from "./components/dashboard/Dashboard";
-import ProductDisplay from "./components/product-list/ProductDisplay";
+import ProductDisplay from "./components/productList/ProductDisplay";
 // Check for token to keep user logged in
 if (localStorage.jwtToken) {
     // Set auth token header auth
