f672c785d7bab9696a8cf762b45baf7298e629f3
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-19 13:37:00 -0300

    refactor code to remove tailwind

--- client/src/components/productList/ProductList.js
+++ client/src/components/productList/ProductList.js
@@ -5,28 +5,51 @@ const ProductList = ({filteredProductList, loading}) => {
         return <>Loading</> // use your loading state or component
 
     return (
-        <div className="w-full h-[85%] px-5">
-            <div className="w-full">Anúncios: </div>
-            <div className="w-full h-full flex flex-wrap gap-1 justify-between items-start overflow-y-auto">
-                {
-                    filteredProductList.map((product) => (
-                        <div key={product.id} className='w-[19%] h-fit my-3 rounded-xl overflow-hidden border border-gray-200'>
-                            <img
-                                src={product.thumbnail}
-                                alt='product'
-                                className='w-full h-28 object-cover'
-                            />
-                            <div className="mt-2 mb-2 px-3">
-                                <div className="font-semibold">
-                                    {(product.title.length > 25)? product.title.substring(0,22) + '...': product.title}
-                                </div>
-                                <div className="text-sm text-gray-600">
-                                    {product.category}
-                                </div>
+        <div style={{ width: '100%', height: '85%', padding: '5px' }}>
+            <div style={{ width: '100%' }}>Anúncios: </div>
+            <div
+                style={{
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    gap: '1px',
+                    justifyContent: 'space-between',
+                    alignItems: 'flex-start',
+                    overflowY: 'auto'
+                }}
+            >
+                {filteredProductList.map((product) => (
+                    <div
+                        key={product.id}
+                        style={{
+                            width: '19%',
+                            height: 'fit-content',
+                            margin: '3px 0',
+                            borderRadius: '8px',
+                            overflow: 'hidden',
+                            border: '1px solid #E5E7EB'
+                        }}
+                    >
+                        <img
+                            src={product.thumbnail}
+                            alt='product'
+                            style={{
+                                width: '100%',
+                                height: '7rem',
+                                objectFit: 'cover'
+                            }}
+                        />
+                        <div style={{ marginTop: '0.5rem', marginBottom: '0.5rem', padding: '0.75rem' }}>
+                            <div style={{ fontWeight: '600' }}>
+                                {product.title.length > 25 ? product.title.substring(0, 22) + '...' : product.title}
+                            </div>
+                            <div style={{ fontSize: '0.875rem', color: '#4B5563' }}>
+                                {product.category}
                             </div>
                         </div>
-                    ))
-                }
+                    </div>
+                ))}
             </div>
         </div>
     )
