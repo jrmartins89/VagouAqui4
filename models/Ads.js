1594e6dce8740eae435918d3a58a3f9c8b6164c8
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-23 15:58:17 -0300

    added thumbnail property to schema

--- models/Ads.js
+++ models/Ads.js
@@ -1,10 +1,10 @@
-// adsSchema.js
 const mongoose = require('mongoose');
 
 const adSchema = new mongoose.Schema({
     title: String,
     link: String,
-    description: String
+    description: String,
+    thumbnail: String
 });
 
 const Ad = mongoose.model('Ad', adSchema);
