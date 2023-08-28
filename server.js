9a7a353ccf9b0926367fd570d049364f1b9c719f
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-28 20:49:10 -0300

    refactored code to include 'neighbourhood' property in database and moved urls to a json file.

--- server.js
+++ server.js
@@ -7,20 +7,12 @@ const users = require("./routes/api/users");
 const app = express();
 const scraper = require("./scrapers/classificadosUfscScraper");
 const Ad = require("./models/Ads");
+const urls = require("./urls.json"); // Load URLs from the JSON file
 require("dotenv").config();
 require("./config/passport")(passport);
-const urls = [
-    "https://classificados.inf.ufsc.br/index.php?catid=88", // carvoeira
-    "https://classificados.inf.ufsc.br/index.php?catid=91", // corrego grande
-    "https://classificados.inf.ufsc.br/index.php?catid=89", // serrinha
-    "https://classificados.inf.ufsc.br/index.php?catid=94", // outros bairros
-    "https://classificados.inf.ufsc.br/index.php?catid=197", // centro
-    "https://classificados.inf.ufsc.br/index.php?catid=90", // itacorubi
-    "https://classificados.inf.ufsc.br/index.php?catid=96", // saco dos limoes
-    "https://classificados.inf.ufsc.br/index.php?catid=86" // trindade
-];
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 mongoose
     .connect(process.env.MONGODB_URI, {
         useNewUrlParser: true,
@@ -43,8 +35,8 @@ app.use("/api/users", users);
 // Start scraping function
 async function startScraping() {
     try {
-        for (const url of urls) {
-            const adItems = await scraper.getAdLinks(url);
+        for (const urlInfo of urls) {
+            const adItems = await scraper.getAdLinks(urlInfo.url);
             const existingAds = await Ad.find({}, "link");
 
             const newAdItems = adItems.filter(
@@ -60,12 +52,13 @@ async function startScraping() {
                     description: item.description,
                     price: item.price,
                     imageLinks: item.imageLinks,
+                    neighbourhood: urlInfo.neighbourhood, // Save neighbourhood value
                 }));
 
                 await Ad.insertMany(finalItems);
-                console.log(`Scraped data from ${url} has been saved to MongoDB collection "ads"`);
+                console.log(`Scraped data from ${urlInfo.neighbourhood} has been saved to MongoDB collection "ads"`);
             } else {
-                console.log(`No new ads to save from ${url}`);
+                console.log(`No new ads to save from ${urlInfo.neighbourhood}`);
             }
         }
     } catch (error) {
