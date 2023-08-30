7f62fd76518449807132acfc6b1738e8c04441fd
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-30 00:20:35 -0300

    changed route name

--- routes/api/ads.js
+++ routes/api/ads.js
@@ -5,7 +5,7 @@ const Ad = require("../../models/Ads");
 // @route GET api/ads
 // @desc Retrieve all ads
 // @access Public
-router.get("/", (req, res) => {
+router.get("/all", (req, res) => {
     Ad.find()
         .then(ads => res.json(ads))
         .catch(err => res.status(500).json({ error: "Error retrieving ads" }));
