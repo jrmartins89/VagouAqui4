bbd1babad10792a9b6dd282baaef42b3428a1542
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-08-31 15:24:29 -0300

    added filter filed to display ads based on neighborhoods

--- client/src/components/adGrid/AdGrid.js
+++ client/src/components/adGrid/AdGrid.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
-import './AdGrid.css'; // Import the CSS file for styling
+import './AdGrid.css';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-
+import Select from 'react-select';
 
 function AdGrid() {
     const [ads, setAds] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [adsPerPage] = useState(5);
+    const [selectedNeighborhood, setSelectedNeighborhood] = useState(null); // Selected neighborhood filter
 
     useEffect(() => {
         async function fetchAds() {
@@ -22,41 +23,68 @@ function AdGrid() {
         fetchAds();
     }, []);
 
+    const neighborhoods = Array.from(new Set(ads.map(ad => ad.neighbourhood))); // Get unique neighborhoods
+    const filteredAds = selectedNeighborhood
+        ? ads.filter(ad => ad.neighbourhood === selectedNeighborhood.value)
+        : ads;
     const indexOfLastAd = currentPage * adsPerPage;
     const indexOfFirstAd = indexOfLastAd - adsPerPage;
-    const currentAds = ads.slice(indexOfFirstAd, indexOfLastAd);
+    const currentAds = filteredAds.slice(indexOfFirstAd, indexOfLastAd);
 
     const paginate = pageNumber => setCurrentPage(pageNumber);
 
+    const neighborhoodOptions = neighborhoods.map(neighborhood => ({
+        value: neighborhood,
+        label: neighborhood,
+    }));
+
+    const handleNeighborhoodChange = selectedOption => {
+        setSelectedNeighborhood(selectedOption);
+        setCurrentPage(1); // Reset page number when neighborhood filter changes
+    };
+
     return (
-        <div className="grid-container">
-            <div className="grid">
-                {currentAds.map(ad => (
-                    <div key={ad._id} className="grid-item">
-                        <h2>{ad.title}</h2>
-                        <Carousel showArrows={true} infiniteLoop={true}>
-                            {ad.imageLinks.map((imageLink, index) => (
-                                <div key={index}>
-                                    <img src={imageLink} alt={`Ad ${index}`} className="ad-image" />
-                                </div>
-                            ))}
-                        </Carousel>
-                        <p className="ad-description">{ad.description}</p>
-                        <p className="ad-price">{ad.price}</p>
-                        <p className="ad-neighbourhood">Bairro: {ad.neighbourhood}</p>
-                    </div>
-                ))}
-            </div>
-            <div className="pagination">
-                {Array.from({ length: Math.ceil(ads.length / adsPerPage) }).map((_, index) => (
-                    <button
-                        key={index}
-                        onClick={() => paginate(index + 1)}
-                        className={currentPage === index + 1 ? 'active' : ''}
-                    >
-                        {index + 1}
-                    </button>
-                ))}
+        <div>
+            <section className="filtros">
+                <div className="filtro-item">
+                    <label>Bairro:</label>
+                    <Select
+                        options={neighborhoodOptions}
+                        value={selectedNeighborhood}
+                        onChange={handleNeighborhoodChange}
+                        isClearable
+                    />
+                </div>
+            </section>
+            <div className="grid-container">
+                <div className="grid">
+                    {currentAds.map(ad => (
+                        <div key={ad._id} className="grid-item">
+                            <h2>{ad.title}</h2>
+                            <Carousel showArrows={true} infiniteLoop={true}>
+                                {ad.imageLinks.map((imageLink, index) => (
+                                    <div key={index}>
+                                        <img src={imageLink} alt={`Ad ${index}`} className="ad-image" />
+                                    </div>
+                                ))}
+                            </Carousel>
+                            <p className="ad-description">{ad.description}</p>
+                            <p className="ad-price">{ad.price}</p>
+                            <p className="ad-neighbourhood">Bairro: {ad.neighbourhood}</p>
+                        </div>
+                    ))}
+                </div>
+                <div className="pagination">
+                    {Array.from({ length: Math.ceil(filteredAds.length / adsPerPage) }).map((_, index) => (
+                        <button
+                            key={index}
+                            onClick={() => paginate(index + 1)}
+                            className={currentPage === index + 1 ? 'active' : ''}
+                        >
+                            {index + 1}
+                        </button>
+                    ))}
+                </div>
             </div>
         </div>
     );
