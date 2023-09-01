9266849f5a59b50df73df646a97be634d9eb039a
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-09-01 17:26:21 -0300

    saved title and description from ads

--- scrapers/ibagyScraper.js
+++ scrapers/ibagyScraper.js
@@ -26,8 +26,37 @@ async function scrapeIbagyAds() {
             fs.writeFileSync('adsLinks.json', JSON.stringify(adsData, null, 2));
 
             console.log('Unique ads links saved to adsLinks.json');
+
+            // Call the function to scrape ad details
+            await scrapeIbagyAdsDetails(uniqueAdsLinks);
         } else {
             console.error('Failed to fetch the page. Status code:', response.status);
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
+
+async function scrapeIbagyAdsDetails(adLinks) {
+    try {
+        for (const link of adLinks) {
+            const response = await axios.get(link);
+
+            if (response.status === 200) {
+                const $ = cheerio.load(response.data);
+                const description = $('#clb-descricao').html();
+
+                const titleMatch = description.match(/<h2>(.*?)<\/h2>/);
+                const title = titleMatch ? titleMatch[1] : 'Title not found';
+
+                const adDescriptionMatch = description.match(/clb-descricao > div > div > div:nth-child(3) > p/);
+                const adDescription = adDescriptionMatch ? $(adDescriptionMatch[0]).text() : 'Description not found';
+
+                console.log('Title:', title);
+                console.log('Ad Description:', adDescription);
+            } else {
+                console.error('Failed to fetch ad details from:', link);
+            }
         }
     } catch (error) {
         console.error('Error:', error.message);
