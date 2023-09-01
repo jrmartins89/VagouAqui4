3c84207fb67314df3f0f18fae56c1ec14fa6d23d
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-09-01 16:02:59 -0300

    removed incorrectly declared function

--- scrapers/classificadosUfscScraper.js
+++ scrapers/classificadosUfscScraper.js
@@ -1,4 +1,4 @@
-const { scrapeImagesClassificadosUfsc, scrapeImagesIbagy }  = require('./imageScraper');
+const { scrapeImagesClassificadosUfsc }  = require('./imageScraper');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
