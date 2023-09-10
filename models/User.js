b671afa63f29b81d5d7480e54fbfed212c4b6fb7
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-09-10 18:25:29 -0300

    created preferences property

--- models/User.js
+++ models/User.js
@@ -1,6 +1,58 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-// Create Schema
+
+const PreferencesSchema = new Schema({
+    houseOrApartment: {
+        type: String,
+        enum: ["House", "Apartment"],
+        default: "Apartment"
+    },
+    genderPreference: {
+        type: String,
+        enum: ["Men", "Women", "Any"],
+        default: "Any"
+    },
+    acceptsPets: {
+        type: Boolean,
+        default: false
+    },
+    location: {
+        type: String,
+        default: ""
+    },
+    roommates: {
+        type: String,
+        enum: ["Alone", "With Roommates"],
+        default: "Alone"
+    },
+    amenities: {
+        type: String,
+        default: ""
+    },
+    leaseLength: {
+        type: String,
+        default: ""
+    },
+    budget: {
+        type: String,
+        default: ""
+    },
+    securityDeposit: {
+        type: String,
+        default: ""
+    },
+    wheelchairAccessible: {
+        type: Boolean,
+        default: false
+    },
+    noiseLevel: {
+        type: String,
+        enum: ["Quiet", "Social"],
+        default: "Quiet"
+    }
+    // Add more preference fields as needed
+});
+
 const UserSchema = new Schema({
     name: {
         type: String,
@@ -17,6 +69,8 @@ const UserSchema = new Schema({
     date: {
         type: Date,
         default: Date.now
-    }
+    },
+    preferences: PreferencesSchema // Embed the preferences schema
 });
+
 module.exports = User = mongoose.model("users", UserSchema);
