71aaccf1b5f57286e6c2a53f0c5fcc624f24d914
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-09-13 23:53:02 -0300

    updated scraper function

--- scrapers/webQuartoScrapper.js
+++ scrapers/webQuartoScrapper.js
@@ -1,2 +1,21 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+
+
+async function scrapeWebQuartoads() {
+    try {
+        const response = await axios.get('https://www.webquarto.com.br/busca/quartos/florianopolis-sc');
+
+        if (response.status === 200) {
+            const $ = cheerio.load(response.data);
+        } else {
+            console.error('Failed to fetch the page. Status code:', response.status);
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+}
+
+module.exports = {
+    scrapeWebQuartoads
+};
