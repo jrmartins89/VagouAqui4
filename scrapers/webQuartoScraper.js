561aeace1e66322c7f64e2c62e99641fe11d586b
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-09-14 00:44:59 -0300

    updated scraper function

--- scrapers/webQuartoScraper.js
+++ scrapers/webQuartoScraper.js
@@ -1,7 +1,6 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-
 async function scrapeWebQuartoads() {
     try {
         const response = await axios.get('https://www.webquarto.com.br/busca/quartos/florianopolis-sc');
@@ -9,21 +8,6 @@ async function scrapeWebQuartoads() {
         if (response.status === 200) {
             const $ = cheerio.load(response.data);
             const adsPage = $('#ssr_root > div:nth-child(3) > div:nth-child(3) > div');
-
-            const adsLinks = new Set(); // Use a Set to store unique links
-
-            adsPage.find('a[target="_blank"]').each((index, element) => {
-                const link = $(element).attr('href');
-                if (link) {
-                    console.log(link);
-                    adsLinks.add(link); // Add the link to the Set
-                }
-            });
-
-            const uniqueAdsLinks = Array.from(adsLinks); // Convert the Set back to an array
-
-            const adsData = { links: uniqueAdsLinks };
-            console.log('LINKS >>>>>>>>>>>>>>>>>', uniqueAdsLinks);
 
         } else {
             console.error('Failed to fetch the page. Status code:', response.status);
