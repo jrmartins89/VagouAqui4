0ceeabba95ae0d4fba64f48d4b35e37d741cd959
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-09-14 00:03:39 -0300

    updated scraper function

--- scrapers/webQuartoScraper.js
+++ scrapers/webQuartoScraper.js
@@ -8,6 +8,17 @@ async function scrapeWebQuartoads() {
 
         if (response.status === 200) {
             const $ = cheerio.load(response.data);
+            const adsPage = $('#ssr_root > div:nth-child(3) > div:nth-child(3) > div');
+
+            const adsLinks = new Set(); // Use a Set to store unique links
+
+            adsPage.find('a[target="_blank"]').each((index, element) => {
+                const link = $(element).attr('href');
+                if (link) {
+                    console.log(link);
+                    adsLinks.add(link); // Add the link to the Set
+                }
+            });
         } else {
             console.error('Failed to fetch the page. Status code:', response.status);
         }
