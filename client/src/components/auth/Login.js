fc736ad4084292dc100de694a5c6f79b7dfd2768
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-09-17 13:40:15 -0300

    created css file and updated component with the css style

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -4,6 +4,8 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { loginUser } from "../../actions/authActions";
 import classnames from "classnames";
+import "./Login.css"; // Import the CSS file
+
 class Login extends Component {
     constructor() {
         super();
@@ -15,14 +17,14 @@ class Login extends Component {
     }
 
     componentDidMount() {
-        // If logged in and user navigates to Login page, should redirect them to dashboard
         if (this.props.auth.isAuthenticated) {
             this.props.history.push("/dashboard");
         }
     }
+
     componentWillReceiveProps(nextProps) {
         if (nextProps.auth.isAuthenticated) {
-            this.props.history.push("/dashboard"); // push user to dashboard when they login
+            this.props.history.push("/dashboard");
         }
         if (nextProps.errors) {
             this.setState({
@@ -30,31 +32,31 @@ class Login extends Component {
             });
         }
     }
-    onChange = e => {
+
+    onChange = (e) => {
         this.setState({ [e.target.id]: e.target.value });
     };
-    onSubmit = e => {
+
+    onSubmit = (e) => {
         e.preventDefault();
         const userData = {
             email: this.state.email,
             password: this.state.password
         };
-        this.props.loginUser(userData); // since we handle the redirect within our component, we don't need to pass in this.props.history as a parameter
+        this.props.loginUser(userData);
     };
+
     render() {
         const { errors } = this.state;
         return (
             <div className="container">
-                <div style={{ marginTop: "4rem" }} className="row">
+                <div className="row">
                     <div className="col s8 offset-s2">
                         <Link to="/" className="btn-flat waves-effect">
-                            <i className="material-icons left">keyboard_backspace</i> Back to
-                            home
+                            <i className="material-icons left">keyboard_backspace</i> Back to home
                         </Link>
-                        <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                            <h4>
-                                <b>Login</b> below
-                            </h4>
+                        <div className="col s12 heading">
+                            <h4>Login below</h4>
                             <p className="grey-text text-darken-1">
                                 Don't have an account? <Link to="/register">Register</Link>
                             </p>
@@ -64,7 +66,7 @@ class Login extends Component {
                                 <input
                                     onChange={this.onChange}
                                     value={this.state.email}
-                                    error={errors.email}
+                                    error={errors.email || errors.emailnotfound}
                                     id="email"
                                     type="email"
                                     className={classnames("", {
@@ -81,7 +83,7 @@ class Login extends Component {
                                 <input
                                     onChange={this.onChange}
                                     value={this.state.password}
-                                    error={errors.password}
+                                    error={errors.password || errors.passwordincorrect}
                                     id="password"
                                     type="password"
                                     className={classnames("", {
@@ -94,16 +96,10 @@ class Login extends Component {
                                     {errors.passwordincorrect}
                 </span>
                             </div>
-                            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                            <div className="col s12">
                                 <button
-                                    style={{
-                                        width: "150px",
-                                        borderRadius: "3px",
-                                        letterSpacing: "1.5px",
-                                        marginTop: "1rem"
-                                    }}
                                     type="submit"
-                                    className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                                    className="btn-large waves-effect waves-light hoverable blue accent-3"
                                 >
                                     Login
                                 </button>
@@ -115,16 +111,16 @@ class Login extends Component {
         );
     }
 }
+
 Login.propTypes = {
     loginUser: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     errors: PropTypes.object.isRequired
 };
-const mapStateToProps = state => ({
+
+const mapStateToProps = (state) => ({
     auth: state.auth,
     errors: state.errors
 });
-export default connect(
-    mapStateToProps,
-    { loginUser }
-)(Login);
+
+export default connect(mapStateToProps, { loginUser })(Login);
