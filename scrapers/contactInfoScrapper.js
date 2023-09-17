8642bb031fc412216549fd5c1917ba2228033198
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-09-17 18:57:21 -0300

    refactored again to save only unique contactInfo

--- scrapers/contactInfoScrapper.js
+++ scrapers/contactInfoScrapper.js
@@ -1,20 +1,17 @@
 function extractContactInfoFromDescription(description) {
     const phonePatterns = [
-        /\(\d{2}\) \d{9}/,                 // (xx) xxxxxxxxx
-        /\d{2}-\d{9}/,                    // xx-xxxxxxxxx
-        /\+\d{2} \d{2} \d{9}/,            // +xx xx xxxxxxxxx
-        /\d{2} \d{5}-\d{4}/,              // xx xxxxx-xxxx
-        /\+\d{2} \d{2} \d{5}-\d{4}/,      // +xx xx xxxxx-xxxx
-        /\d{2} \d{8}/,                    // xx xxxxxxxxx
-        /\(\+\d{2}\) \d{2} \d{9}/,        // (+xx) xx xxxxxxxxx
-        /\+\d{2} \d{2} \d{4}-\d{4}/,      // +xx xx xxxx-xxxx
-        /\d{10}/,                         // xxxxxxxxxxx
-        /\d{2}\.\d\.\d{4}\.\d{4}/,        // xx.x.xxxx.xxxx
-        /\(\d{2}\) \d{5}-\d{4}/,          // (xx) xxxxx-xxxx
-        /\d{2} \d \d{4} \d{4}/,           // xx x xxxx xxxx
-        /\(\d{2}\)\d{5}-\d{4}/,           // (xx)xxxxx-xxxx
-        /\+\d{2}\(\d{2}\)\d{5}-\d{4}/,     // +xx(xx)xxxxx-xxxx
-        /\(\d{2}\)\d{9}/                  // (xx)xxxxxxxxx
+        /\+\d{2}\(\d{2}\)\d{5}-\d{4}/, // +xx(xx)xxxxx-xxxx
+        /\+\d{2} \d{2} \d{9}/,        // +xx xx xxxxxxxxx
+        /\+\d{2} \d{2} \d{4}-\d{4}/,  // +xx xx xxxx-xxxx
+        /\(\+\d{2}\) \d{2} \d{9}/,    // (+xx) xx xxxxxxxxx
+        /\(\d{2}\)\d{9}/,             // (xx)xxxxxxxxx
+        /\(\d{2}\) \d{5}-\d{4}/,      // (xx) xxxxx-xxxx
+        /\d{2} \d{5}-\d{4}/,          // xx xxxxx-xxxx
+        /\d{2} \d{8}/,                // xx xxxxxxxxx
+        /\d{2}-\d{9}/,                // xx-xxxxxxxxx
+        /\d{2} \d \d{4} \d{4}/,       // xx x xxxx xxxx
+        /\d{10}/,                     // xxxxxxxxxxx
+        /\d{2}\.\d\.\d{4}\.\d{4}/     // xx.x.xxxx.xxxx
     ];
 
     const uniqueContactInfo = new Set(); // Use a Set to store unique phone numbers
@@ -22,9 +19,8 @@ function extractContactInfoFromDescription(description) {
     phonePatterns.forEach(pattern => {
         const matches = description.match(pattern);
         if (matches) {
-            matches.forEach(match => {
-                uniqueContactInfo.add(match + ''); // Add each match to the Set
-            });
+            // Since we only want to save unique matches, add the first match found
+            uniqueContactInfo.add(matches[0]);
         }
     });
 
