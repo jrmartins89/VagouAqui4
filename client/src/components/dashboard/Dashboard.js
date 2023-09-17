898df819a6da8e6cfa9f6ca86982e9aa98cacfdb
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-09-17 13:33:57 -0300

    created css file for dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,8 +1,11 @@
+// Dashboard.js
+
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 import { Link } from "react-router-dom";
+import "./Dashboard.css"; // Import the CSS file
 
 class Dashboard extends Component {
     onLogoutClick = (e) => {
@@ -14,44 +17,34 @@ class Dashboard extends Component {
         const { user } = this.props.auth;
 
         return (
-            <div style={{ height: "75vh" }} className="container valign-wrapper">
+            <div className="container">
                 <div className="row">
                     <div className="col s12 center-align">
                         <h4>
                             <b>Hey there,</b> {user.name.split(" ")[0]}
-                            <p className="flow-text grey-text text-darken-1">
-                                You are logged into
-                                <span style={{ fontFamily: "monospace" }}> VagouAqui</span> 👏
-                            </p>
                         </h4>
-                        <div className="col s6">
-                            <Link
-                                to="/products"
-                                style={{
-                                    width: "140px",
-                                    borderRadius: "3px",
-                                    letterSpacing: "1.5px",
-                                }}
-                                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                            >
-                                Produtos
-                            </Link>
-                        </div>
-                        <div className="col s6">
-                            <button
-                                style={{
-                                    width: "140px",
-                                    borderRadius: "3px",
-                                    letterSpacing: "1.5px",
-                                    backgroundColor: "red",
-                                    color: "white",
-                                }}
-                                onClick={this.onLogoutClick}
-                                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                            >
-                                Logout
-                            </button>
-                        </div>
+                        <p className="flow-text grey-text text-darken-1">
+                            You are logged into
+                            <span className="monospace-text"> VagouAqui</span> 👏
+                        </p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col s6">
+                        <Link
+                            to="/products"
+                            className="btn produtos-btn waves-effect waves-light hoverable"
+                        >
+                            Produtos
+                        </Link>
+                    </div>
+                    <div className="col s6">
+                        <button
+                            onClick={this.onLogoutClick}
+                            className="btn logout-btn waves-effect waves-light hoverable"
+                        >
+                            Logout
+                        </button>
                     </div>
                 </div>
             </div>
