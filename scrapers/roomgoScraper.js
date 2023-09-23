b489add5f7e6bdf5bf6909782c23abb665e9cf95
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-09-23 13:42:26 -0300

    refactored code to get ad description

--- scrapers/roomgoScraper.js
+++ scrapers/roomgoScraper.js
@@ -1,23 +1,48 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const { extractContactInfoFromDescription } = require("./contactInfoScrapper");
 
+// Function to scrape Roomgo ad details
+async function getRoomgoAdDetails(adLink) {
+    try {
+        const response = await axios.get(adLink);
+
+        if (response.status === 200) {
+            const $ = cheerio.load(response.data);
+
+            // Find the main content of the ad
+            const adContent = $('#roomDetail > div.page.page-room-detail > div.grid-wrapper.grid-12 > div.grid-box.span-7 > div.main-content');
+
+            // Find the description text inside adContent
+            const adDescription = adContent.find('div.content-block.description-text p').text();
+
+            // Display the ad description in the console
+            console.log("Ad Description:");
+            console.log(adDescription);
+
+            return adDescription;
+        } else {
+            console.error(`Failed to fetch ad details from ${adLink}. Status code: ${response.status}`);
+        }
+    } catch (error) {
+        console.error(`Error while scraping ad details from ${adLink}:`, error.message);
+    }
+}
+
+// Function to scrape Roomgo ad links
 async function scrapeRoomgoAdsPage(pageNumber) {
     try {
-        const url = `https://www.roomgo.com.br/santa-catarina/florianopolis-companheiros-de-quarto?page=1#room-ads-area`;
+        const url = `https://www.roomgo.com.br/santa-catarina/florianopolis-companheiros-de-quarto?page=${pageNumber}#room-ads-area`;
         const response = await axios.get(url);
 
         if (response.status === 200) {
             const $ = cheerio.load(response.data);
-            const ads = [];
-
-            const adLinks = []; // Variable to store ad links
+            const adLinks = [];
 
             // Extract data-url values from elements with the listing_item class
             $('.listing_item').each((index, element) => {
                 const dataUrl = $(element).attr('data-url');
                 if (dataUrl) {
-                    adLinks.push('https://www.roomgo.com.br/santa-catarina'+ dataUrl);
+                    adLinks.push('https://www.roomgo.com.br/santa-catarina' + dataUrl);
                 }
             });
 
@@ -26,8 +51,7 @@ async function scrapeRoomgoAdsPage(pageNumber) {
             console.log(adLinks);
 
             // You can continue with the rest of your scraping logic here
-
-            return ads;
+            return adLinks;
         } else {
             console.error(`Failed to fetch the HTML data for page ${pageNumber}. Status code: ${response.status}`);
         }
@@ -37,17 +61,18 @@ async function scrapeRoomgoAdsPage(pageNumber) {
 }
 
 module.exports = {
-    scrapeRoomgoAdsPage
+    scrapeRoomgoAdsPage,
+    getRoomgoAdDetails
 };
 
-scrapeRoomgoAdsPage();
-
-
 // Example usage:
-// scrapeRoomgoAdsPage(1)
-//   .then((ads) => {
-//       console.log(ads);
-//   })
-//   .catch((error) => {
-//       console.error(error);
-//   });
+scrapeRoomgoAdsPage(1)
+   .then((adLinks) => {
+       // Now you can iterate through adLinks and call getRoomgoAdDetails for each ad
+       adLinks.forEach((adLink) => {
+           getRoomgoAdDetails(adLink);
+       });
+   })
+   .catch((error) => {
+       console.error(error);
+   });
