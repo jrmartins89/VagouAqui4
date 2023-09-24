1083f43bf2d56c524cd2781766f7dafc53fbdb12
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-09-23 23:22:20 -0300

    get adtitle,addescription, adprice and adlink

--- scrapers/mgfScraper.js
+++ scrapers/mgfScraper.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 // Function to scrape and display href values from VivaReal ad page
-async function extractMgfAdLinkValues() {
+async function extractMgfHrefValues() {
     try {
         const response = await axios.get('https://www.mgfimoveis.com.br/aluguel/kitnet/sc-florianopolis?page=1');
         const adLinks = [];
@@ -15,19 +15,52 @@ async function extractMgfAdLinkValues() {
                 const adLink = $(element).find('a.h-100.d-flex.flex-column').attr('href');
                 if (adLink) {
                     adLinks.push(adLink);
-                    console.log('Ad Link:', adLink);
                 }
             });
+
+            // Call extractMgfAdDetails with adLinks
+            const adDetails = await extractMgfAdDetails(adLinks);
+            console.log(JSON.stringify(adDetails, null, 2));
         }
-        return adLinks;
     } catch (error) {
         console.error('Error while scraping href values for ads:', error.message);
     }
 }
 
+// Function to extract ad details
+async function extractMgfAdDetails(adLinks) {
+    const adDetails = [];
+
+    for (const adLink of adLinks) {
+        try {
+            const response = await axios.get(adLink);
+            if (response.status === 200) {
+                const $ = cheerio.load(response.data);
+                const adTitle = $('body > main > div.row.justify-content-center > article > div:nth-child(2) > header > h1').text().trim();
+                const adDescription = $('#dbox > p').text().trim();
+                const adPrice = $('body > main > div.row.justify-content-center > article > div:nth-child(2) > div:nth-child(2) > div > div.card.border-secondary.rounded-4.shadow.mb-4.p-3 > h3').text().trim();
+
+                // Create an object to store ad details
+                const adDetail = {
+                    adTitle,
+                    adDescription,
+                    adPrice,
+                    adLink
+                };
+
+                adDetails.push(adDetail);
+            }
+        } catch (error) {
+            console.error('Error while scraping ad details:', error.message);
+        }
+    }
+
+    return adDetails;
+}
+
 module.exports = {
-    extractMgfAdLinkValues
+    extractMgfHrefValues
 }
 
 // Call the function to start scraping and displaying href values
-extractMgfAdLinkValues();
+extractMgfHrefValues();
