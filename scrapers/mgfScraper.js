22a57bac6b87eb2445ebc139d74a97ebe8f35c4e
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-09-23 22:03:18 -0300

    get adList

--- scrapers/mgfScraper.js
+++ scrapers/mgfScraper.js
@@ -2,12 +2,14 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 // Function to scrape image links from VivaReal ad page
-async function extractVivaRealImageLinks(adLink) {
+async function extractVivaRealImageLinks() {
     try {
         const response = await axios.get('https://www.mgfimoveis.com.br/aluguel/kitnet/sc-florianopolis?page=1');
 
         if (response.status === 200) {
             const $ = cheerio.load(response.data);
+            const adList = $('#slist > div');
+
         }
     } catch (error) {
         console.error('Error while scraping image links for ad' , error.message);
@@ -18,3 +20,4 @@ async function extractVivaRealImageLinks(adLink) {
 module.exports = {
     extractVivaRealImageLinks
 };
+extractVivaRealImageLinks();
