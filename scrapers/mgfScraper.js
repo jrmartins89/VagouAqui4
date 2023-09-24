1699af066cd1d959c560067723f102f3a9d8e1ac
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-09-24 00:40:30 -0300

    refactored the code to make function extractMgfHrefValues faster

--- scrapers/mgfScraper.js
+++ scrapers/mgfScraper.js
@@ -1,14 +1,13 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-// Function to scrape and display href values from VivaReal ad page
+// Function to scrape and display href values from MGF ad page
 async function extractMgfHrefValues() {
     try {
         const baseUrl = 'https://www.mgfimoveis.com.br/aluguel/kitnet/sc-florianopolis?page=';
         const adLinks = [];
 
-        // Loop through pages from 1 to 40
-        for (let pageNumber = 1; pageNumber <= 10; pageNumber++) {
+        const getPageLinks = async (pageNumber) => {
             const url = `${baseUrl}${pageNumber}`;
             const response = await axios.get(url);
 
@@ -16,7 +15,6 @@ async function extractMgfHrefValues() {
                 const $ = cheerio.load(response.data);
                 const adList = $('#slist > div');
 
-                // Parse through all children elements of 'adList'
                 adList.children().each((index, element) => {
                     const adLink = $(element).find('a.h-100.d-flex.flex-column').attr('href');
                     if (adLink) {
@@ -24,23 +22,27 @@ async function extractMgfHrefValues() {
                     }
                 });
             }
+        };
+
+        const pagePromises = [];
+        for (let pageNumber = 1; pageNumber <= 10; pageNumber++) {
+            pagePromises.push(getPageLinks(pageNumber));
         }
 
-        // Call extractMgfAdDetails with adLinks
+        await Promise.all(pagePromises);
+
         const adDetails = await extractMgfAdDetails(adLinks);
         console.log(JSON.stringify(adDetails, null, 2));
 
-        // Inform the user that scraping has finished
         console.log('Scraping has finished.');
-        return adDetails
+        return adDetails;
     } catch (error) {
         console.error('Error while scraping href values for ads:', error.message);
     }
 }
 
-// Function to extract ad details
 async function extractMgfAdDetails(adLinks) {
-    const adDetails = await Promise.all(adLinks.map(async (adLink) => {
+    const adDetailPromises = adLinks.map(async (adLink) => {
         try {
             const response = await axios.get(adLink);
             if (response.status === 200) {
@@ -49,7 +51,6 @@ async function extractMgfAdDetails(adLinks) {
                 const adDescription = $('#dbox > p').text().trim();
                 const adPrice = $('body > main > div.row.justify-content-center > article > div:nth-child(2) > div:nth-child(2) > div > div.card.border-secondary.rounded-4.shadow.mb-4.p-3 > h3').text().trim();
 
-                // Create an object to store ad details
                 const adDetail = {
                     adTitle,
                     adDescription,
@@ -62,14 +63,15 @@ async function extractMgfAdDetails(adLinks) {
         } catch (error) {
             console.error('Error while scraping ad details:', error.message);
         }
-    }));
+    });
 
-    return adDetails.filter(Boolean); // Remove any undefined values
+    const adDetails = await Promise.all(adDetailPromises);
+
+    return adDetails.filter(Boolean);
 }
 
 module.exports = {
     extractMgfHrefValues
 }
 
-// Call the function to start scraping and displaying href values
 extractMgfHrefValues();
