646544900a14cfc43bd9dff9bf3decda83270f6a
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-09-25 19:44:29 -0300

    updated ad router to include route to recommendation system

--- routes/api/ads.js
+++ routes/api/ads.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Ad = require('../../models/Ads'); // Adjust the path as needed
+const { generateContentBasedRecommendations } = require('../../recommendationSystem/recommendation'); // Import the recommendation function
 
 // Route to fetch ads
 router.get('/all', async (req, res) => {
@@ -9,6 +10,20 @@ router.get('/all', async (req, res) => {
         res.json(ads);
     } catch (error) {
         res.status(500).json({ message: 'Error fetching ads', error: error.message });
+    }
+});
+
+// Route to fetch content-based recommendations for a user
+router.get('/recommendations/:userId', async (req, res) => {
+    try {
+        const userId = req.params.userId;
+
+        // Call the generateContentBasedRecommendations function from the recommendation.js file
+        const recommendedAds = await generateContentBasedRecommendations(userId);
+
+        res.json(recommendedAds);
+    } catch (error) {
+        res.status(500).json({ message: 'Error fetching recommendations', error: error.message });
     }
 });
 
