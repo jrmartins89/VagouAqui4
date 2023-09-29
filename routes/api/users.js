889b1b5623b10f55ef280a17ec4bb8709105912d
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-09-29 00:59:13 -0300

    changed declaration place.

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,13 +4,10 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
-
+const User = require("../../models/User");// Load User model
 // Load input validation
 const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
-
-// Load User model
-const User = require("../../models/User");
 
 // @route POST api/users/register
 // @desc Register user
