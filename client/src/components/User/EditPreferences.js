2652b4413d81e2264abecd3401c3fa108ded4310
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-09-28 21:35:33 -0300

    Added more fields to component

--- client/src/components/User/EditPreferences.js
+++ client/src/components/User/EditPreferences.js
@@ -66,7 +66,24 @@ class EditPreferences extends Component {
 
     render() {
         const { preferences } = this.state;
-
+        const fieldIcons = {
+            name: "account_circle",
+            email: "email",
+            password: "lock",
+            password2: "lock",
+            location: "location_on",
+            budget: "attach_money",
+            houseOrApartment: "home",
+            genderPreference: "people",
+            acceptsPets: "pets",
+            roommates: "groups",
+            leaseLength: "event",
+            securityDeposit: "security",
+            wheelchairAccessible: "accessible",
+            noiseLevel: "volume_up",
+            acceptSmoker: "smoking_rooms",
+            // Add more fields and icons as needed
+        };
         return (
             <div className="container">
                 <div className="row">
@@ -102,8 +119,112 @@ class EditPreferences extends Component {
                                 />
                                 <label htmlFor="preferences.budget">Budget (R$):</label>
                             </div>
-                            {/* Render other preference fields */}
-                            {/* ... (other preference fields) ... */}
+                            <div className="input-field col s12 form-field">
+                                <div>
+                                    <label>Prefere casa ou apartamento?</label>
+                                </div>
+                                <select
+                                    id="preferences.houseOrApartment"
+                                    name="houseOrApartment"
+                                    value={preferences.houseOrApartment}
+                                    onChange={this.onChange}
+                                >
+                                    <option value="House">Casa</option>
+                                    <option value="Apartment">Apartamento</option>
+                                </select>
+                            </div>
+                            <div className="input-field col s12 form-field">
+                                <div>
+                                    <label>Procuro anúncios de aluguéis destinados à:</label>
+                                </div>
+                                <i className="material-icons prefix">{fieldIcons.genderPreference}</i>
+                                <select
+                                    id="preferences.genderPreference"
+                                    name="genderPreference"
+                                    value={preferences.genderPreference}
+                                    onChange={this.onChange}
+                                >
+                                    <option value="Men">Homens</option>
+                                    <option value="Women">Mulheres</option>
+                                    <option value="Any">Tanto Faz</option>
+                                </select>
+                            </div>
+                            {/* Add more preference fields here */}
+                            <div className="input-field col s12 form-field">
+                                <div>
+                                    <label>Aluguel compartilhado?</label>
+                                </div>
+                                <i className="material-icons prefix">{fieldIcons.roommates}</i>
+                                <select
+                                    id="preferences.roommates"
+                                    name="roommates"
+                                    value={preferences.roommates}
+                                    onChange={this.onChange}
+                                >
+                                    <option value="Alone">Sozinho</option>
+                                    <option value="With Roommates">Compartilhado</option>
+                                </select>
+                            </div>
+                            <div className="input-field col s12 form-field">
+                                <div>
+                                    <label>Duração do aluguel</label>
+                                </div>
+                                <i className="material-icons prefix">{fieldIcons.leaseLength}</i>
+                                <select
+                                    id="preferences.leaseLength"
+                                    name="leaseLength"
+                                    value={preferences.leaseLength}
+                                    onChange={this.onChange}
+                                >
+                                    <option value="year round">Aluguel anual</option>
+                                    <option value="monthly basis">Aluguel temporada</option>
+                                </select>
+                            </div>
+                            <div className="input-field col s12 form-field">
+                                <p className="checkbox-label">
+                                    <i className="material-icons prefix">{fieldIcons.acceptsPets}</i>
+                                    <label>
+                                        <input
+                                            type="checkbox"
+                                            id="preferences.acceptsPets"
+                                            name="acceptsPets"
+                                            checked={preferences.acceptsPets}
+                                            onChange={this.onChange}
+                                        />
+                                        <span className="checkbox-text">Aceita Pets?</span>
+                                    </label>
+                                </p>
+                            </div>
+                            <div className="input-field col s12 form-field">
+                                <p className="checkbox-label">
+                                    <i className="material-icons prefix">{fieldIcons.wheelchairAccessible}</i>
+                                    <label>
+                                        <input
+                                            type="checkbox"
+                                            id="preferences.wheelchairAccessible"
+                                            name="wheelchairAccessible"
+                                            checked={preferences.wheelchairAccessible}
+                                            onChange={this.onChange}
+                                        />
+                                        <span className="checkbox-text">Acessível à cadeirantes?</span>
+                                    </label>
+                                </p>
+                            </div>
+                            <div className="input-field col s12 form-field">
+                                <p className="checkbox-label">
+                                    <i className="material-icons prefix">{fieldIcons.acceptSmoker}</i>
+                                    <label>
+                                        <input
+                                            type="checkbox"
+                                            id="preferences.acceptSmoker"
+                                            name="acceptSmoker"
+                                            checked={preferences.acceptSmoker}
+                                            onChange={this.onChange}
+                                        />
+                                        <span className="checkbox-text">Fumante?</span>
+                                    </label>
+                                </p>
+                            </div>
                             <div className="col s12 form-container">
                                 <button
                                     className="btn btn-large waves-effect waves-light hoverable btn-update"
