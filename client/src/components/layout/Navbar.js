db26538b374b6313262ab527eea1c38159c07ea9
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-09-29 21:54:55 -0300

    make text bold

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -30,7 +30,7 @@ class Navbar extends Component {
                             onClick={this.onLogoutClick}
                             className="btn btn-logout"
                         >
-                            Logout
+                            <b>Logout</b>
                         </button>
                     </div>
                 </nav>
