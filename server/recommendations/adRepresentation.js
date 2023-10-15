55003610a85e565aaec528fdafa59cc72ec50d92
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-10-15 16:06:08 -0300

    added sample size treatment

--- server/recommendations/adRepresentation.js
+++ server/recommendations/adRepresentation.js
@@ -5,7 +5,20 @@ async function generateRecommendations(userPreferences) {
     try {
         // Fetch all ads from the database
         const ads = await Ad.find({});
+        let sampleSize; // Default sample size
 
+        const totalAds = ads.length;
+
+        // Adjust the sample size based on various factors
+        if (totalAds <= 50) {
+            sampleSize = totalAds; // Use all ads if there are very few.
+        } else if (totalAds <= 500) {
+            sampleSize = 100; // Limit the sample size if there are between 6 and 10 ads.
+        } else if (totalAds <= 1000) {
+            sampleSize = 250; // A larger sample size for more diverse content.
+        } else {
+            sampleSize = 300; // For larger databases, a reasonable sample size.
+        }
         // Initialize an array to store recommended ads
         const recommendations = [];
 
@@ -45,7 +58,7 @@ async function generateRecommendations(userPreferences) {
         recommendations.sort((a, b) => b.score - a.score);
 
         // Return the recommended ads as a JSON object
-        return recommendations;
+        return recommendations.slice(0, sampleSize);
     } catch (error) {
         console.error('Error fetching ads or generating recommendations:', error);
         return [];
