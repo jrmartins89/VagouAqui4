1467ccb14d0a8923f658635aaaf2ebcf24cf3ed1
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-10-23 19:35:34 -0300

    refactored component and changed directory

---
+++ client/src/views/layout/MainPage.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux"; // Import connect from React Redux
+import "./MainPage.css"; // Import the CSS file
+
+class MainPage extends Component {
+    render() {
+        const { isAuthenticated } = this.props.auth;
+
+        return (
+            <div className="container-valign container valign-wrapper">
+                <div className="row">
+                    <div className="col s12 center-align">
+                        <h4 className="title">
+                            <b>Página Principal</b>
+                        </h4>
+                        <p className="description">
+                            Portal de anúncios de aluguéis em Florianópolis indexados a partir de técnicas de WebScrapping
+                        </p>
+                        <p className="flow-text grey-text text-darken-1">INE - UFSC - 2023</p>
+                        <br />
+                        {isAuthenticated ? (
+                            <div className="col s6">
+                                <Link
+                                    to="/products"
+                                    className="btn button btn-produtos waves-effect waves-light hoverable"
+                                >
+                                    Anúncios
+                                </Link>
+                            </div>
+                        ) : (
+                            <div className="col s6">
+                                <Link
+                                    to="/register"
+                                    className="btn button btn-cadastro waves-effect waves-light hoverable"
+                                >
+                                    Cadastro
+                                </Link>
+                            </div>
+                        )}
+                        <div className="col s6">
+                            <Link
+                                to="/login"
+                                className="btn button btn-login waves-effect waves-light hoverable"
+                            >
+                                Log In
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    auth: state.auth,
+});
+
+export default connect(mapStateToProps)(MainPage); // Connect the component to Redux
