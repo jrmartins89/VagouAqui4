26934278d7e2ee4cb40048d9364c8e2d65296c3a
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-11-02 18:59:45 -0300

    created route to fetch the last scraping date.

--- server/routes/api/ads.js
+++ server/routes/api/ads.js
@@ -12,4 +12,18 @@ router.get('/all', async (req, res) => {
     }
 });
 
+// New route to fetch LAST_SCRAPING_DATE
+router.get('/lastScrapingDate', (req, res) => {
+    try {
+        const lastScrapingDate = process.env.LAST_SCRAPING_DATE;
+        if (lastScrapingDate) {
+            res.json({ lastScrapingDate });
+        } else {
+            res.status(404).json({ message: 'LAST_SCRAPING_DATE not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Erro ao buscar LAST_SCRAPING_DATE', error: error.message });
+    }
+});
+
 module.exports = router;
