e9f66015d9ed674d8c9fc11c1229640ddb868938
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-11-02 20:23:44 -0300

    changed the way the adRecommendation function calculates the budget feature.

--- server/recommendations/adRecommendation.js
+++ server/recommendations/adRecommendation.js
@@ -22,37 +22,45 @@ async function generateRecommendations(userPreferences) {
         // Initialize an array to store recommended ads
         const recommendations = [];
 
+        // Parse the user's budget preference
+        const userBudget = parseFloat(userPreferences.budget);
+
         // Loop through the ads and calculate a score for each ad based on user preferences
         ads.forEach((ad) => {
-            // Extract features from the ad
-            const adFeatures = {
-                houseOrApartment: ad.description.match(/casa|apartamento/i),
-                genderPreference: ad.description.match(/homem|mulher|masculino|feminino|masculina|feminina/i),
-                acceptsPets: ad.description.match(/aceita pets|pets permitidos/i),
-                location: ad.neighborhood,
-                roommates: ad.description.match(/alugo quarto|aluga-se quarto|quarto disponível|quarto compartilhado/i),
-                leaseLength: ad.description.match(/aluguel anual|aluguel mensal|alugo mensal|alugo anual|aluguel temporada/i),
-                budget: ad.description.match(/\bR\$\s?\d{3,}(?:,\d{1,2})?|\$\s?\d{3,}(?:,\d{1,2})?|\d{3,}(?:,\d{1,2})?\b/),
-                wheelchairAccessible: ad.description.match(/acessível a cadeirantes|acesso à cadeirantes|acesso à cadeira de rodas/i),
-                noiseLevel: ad.description.match(/tranquilo|barulhento|local tranquilo|local perto do centro/i),
-                acceptSmoker: ad.description.match(/aceita fumante|fumante permitido/i),
-                hasFurniture:ad.description.match(/mobiliado |tem mobilia|possui móveis| possui moveis|tem algumas mobilias|mobilia inclusa/i)
-            };
+            // Extract budget information from the ad description and convert it to a numeric value
+            const adBudgetMatch = ad.description.match(/\bR\$\s?\d{3,}(?:,\d{1,2})?|\$\s?\d{3,}(?:,\d{1,2})?|\d{3,}(?:,\d{1,2})?\b/);
+            const adBudget = adBudgetMatch ? parseFloat(adBudgetMatch[0].replace(/[^\d.]/g, '')) : null;
 
-            // Calculate a score for the ad based on user preferences
-            let score = 0;
+            if (adBudget === null || adBudget <= userBudget) {
+                // Extract other features from the ad
+                const adFeatures = {
+                    houseOrApartment: ad.description.match(/casa|apartamento/i),
+                    genderPreference: ad.description.match(/homem|mulher|masculino|feminino|masculina|feminina/i),
+                    acceptsPets: ad.description.match(/aceita pets|pets permitidos/i),
+                    location: ad.neighborhood,
+                    roommates: ad.description.match(/alugo quarto|aluga-se quarto|quarto disponível|quarto compartilhado/i),
+                    leaseLength: ad.description.match(/aluguel anual|aluguel mensal|alugo mensal|alugo anual|aluguel temporada/i),
+                    wheelchairAccessible: ad.description.match(/acessível a cadeirantes|acesso à cadeirantes|acesso à cadeira de rodas/i),
+                    noiseLevel: ad.description.match(/tranquilo|barulhento|local tranquilo|local perto do centro/i),
+                    acceptSmoker: ad.description.match(/aceita fumante|fumante permitido/i),
+                    hasFurniture: ad.description.match(/mobiliado|tem mobilia|possui móveis|possui moveis|tem algumas mobilias|mobilia inclusa/i)
+                };
 
-            for (const feature in adFeatures) {
-                if (userPreferences[feature] && adFeatures[feature]) {
-                    score++;
+                // Calculate a score for the ad based on user preferences
+                let score = 0;
+
+                for (const feature in adFeatures) {
+                    if (userPreferences[feature] && adFeatures[feature]) {
+                        score++;
+                    }
                 }
-            }
 
-            // Add the ad and its score to the recommendations
-            recommendations.push({
-                ad,
-                score,
-            });
+                // Add the ad and its score to the recommendations
+                recommendations.push({
+                    ad,
+                    score,
+                });
+            }
         });
 
         // Sort recommendations by score in descending order
