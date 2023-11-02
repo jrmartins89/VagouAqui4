d87300f1972ba9eadf32fe607a70e9adc7fb3ffe
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-11-02 10:24:48 -0300

    updated component

--- client/src/views/User/UserPage.js
+++ client/src/views/User/UserPage.js
@@ -31,13 +31,22 @@ class UserPage extends Component {
 
         return (
             <div className="user-profile">
-                <h2>User Profile</h2>
+                <h2>Perfil</h2>
                 {error && <p className="error">{error}</p>}
                 {user && (
                     <div className="user-profile-info">
-                        <p>Name: {user.name}</p>
+                        <p>Nome: {user.name}</p>
                         <p>Email: {user.email}</p>
-                        {/* Display other user properties as needed */}
+                        <p>Prefiro casa ou apartamento: {user.preferences.houseOrApartment}</p>
+                        <p>Anúncios com colegas de quarto do gênero: {user.preferences.genderPreference}</p>
+                        <p>Anúncios com pets: {user.preferences.acceptsPets ? 'Sim' : 'Não'}</p>
+                        <p>Preferência por anúncios localizados em: {user.preferences.location}</p>
+                        <p>Preferência por anúncios de aluguéis compartilhados: {user.preferences.roommates}</p>
+                        <p>Preferência por anúncios do tipo: {user.preferences.leaseLength}</p>
+                        <p>Preferência por anúncios que aceitem fumante: {user.preferences.acceptSmoker ? 'Sim' : 'Não'}</p>
+                        <p>Preferência por anúncios que possuem mobília: {user.preferences.hasFurniture ? 'Sim' : 'Não'}</p>
+                        <p>Preferência por anúncios de locais com nível de barulho: {user.preferences.noiseLevel}</p>
+                        <p>Preferência por anúncios de locais acessíveis à cadeirantes: {user.preferences.wheelchairAccessible ? 'Sim' : 'Não'}</p>
                     </div>
                 )}
             </div>
