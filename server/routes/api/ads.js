52b2a7385da0577bb6210bd8031af9987bcce96f
Jose Ribamar Junior <jribamarjunior89@gmail.com>
2023-11-02 16:40:43 -0300

    removed default value from route

--- server/routes/api/ads.js
+++ server/routes/api/ads.js
@@ -6,7 +6,7 @@ const Ad = require('../../models/Ads'); // Adjust the path as needed
 router.get('/all', async (req, res) => {
     try {
         const ads = await Ad.find();
-        const lastScrapingDate = process.env.LAST_SCRAPING_DATE || 'N/A'; // Use 'N/A' if the variable is not set
+        const lastScrapingDate = process.env.LAST_SCRAPING_DATE; // Use 'N/A' if the variable is not set
         res.json({ads, lastScrapingDate});
     } catch (error) {
         res.status(500).json({ message: 'Erro ao listar anúncios', error: error.message });
